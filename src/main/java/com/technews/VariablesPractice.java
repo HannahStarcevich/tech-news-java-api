9fd353c2252c4985efe8f940a15ac7684023c124
HannahStarcevich <starcevichh@gmail.com>
2021-05-13 15:44:53 -0700

    created structured database for users, posts, comments and votes

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int num1 = 10;
+        double num2 = 5.5;
+        double sum = num1 + num2;
+
+        String animal = "dog";
+        String combineIntString = num1 + animal;
+
+        System.out.println(sum);
+        System.out.println(combineIntString);
+    }
+}
